31fd8aae0277da985a1a0261f9e6ca9cab1c5ff0
ayeshaqadri <qadria392@gmail.com>
2024-07-12 19:20:13 +0500

    done

---
+++ ass-no-11/app.js
@@ -0,0 +1,36 @@
+// Question-No-1
+// var userFirstName = prompt("Kindly ! Enter your first name")
+// var userLastName = prompt("Kindly ! Enter your last name")
+// var userFullName =(userFirstName + userLastName)
+// alert = prompt("hello! " + userFullName)
+
+// Question-No-2
+// var userFavoraiteMobiles = prompt("Tell us your favoraite mobile phone")
+// var lengths = userFavoraiteMobiles.length
+// var result = alert("My favourite phone is " + userFavoraiteMobiles  +" Length of string " + lengths)
+
+// Question-No-3
+// var city = "Pakistan"
+// var  length = city.indexOf("n")
+// var result = alert("String : PAKISTAN " + " Index of 'n' :" +  length)
+
+// Question-No-4
+// var cityName = "HELLO WORLD" 
+// var length = cityName.indexOf("L")
+// var result = alert("String : HELLO WORLD " + " Last Index of 'L' :" +  length)
+
+// Question-No-5
+// var city = "Pakistan"
+// var  length = city.charAt("3")
+// var result = alert("String : PAKISTAN " + " Index of '3' : " +  length)
+
+// Question-No-6
+
+
+
+
+// Question-No-7
+
+// Question-No-8
+
+// Question-No-9
